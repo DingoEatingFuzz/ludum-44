b5e4d4b1d7b9497c6990d377616db8d7b47b737d
Michael Lange <dingoeatingfuzz@gmail.com>
2019-04-27 03:14:00 -0700

    Weapons prefabs

--- Assets/Scripts/Weapons/WeaponComponent.cs
+++ Assets/Scripts/Weapons/WeaponComponent.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class WeaponComponent : MonoBehaviour
 {
-    [Tooltip("Where this weapon spawns it's projectiles")]
+    [Tooltip("Where this weapon spawns its projectiles")]
     public GameObject ProjectileSpawnLocation;
 
     [Tooltip("Projectile to spawn")]
     public GameObject ProjectileType;
+
+    [Tooltip("Delay between shots, in seconds")]
+    public float ShotDelay = 0.5f;
+
+    [Tooltip("The amount of distance from the ProjectileSpawnLocation a projectile can spawn")]
+    public float SpawnJitter = 0.0f;
+
+    [Tooltip("The amount in degrees a projectile can devaite from perfect accuracy")]
+    public float Inaccuracy = 0.0f;
+
+    private float Cooldown = 0.0f;
+    private bool CooldownIsActive = false;
 
     /// <summary>
     /// Awake
@@ -23,6 +35,36 @@ public class WeaponComponent : MonoBehaviour
         if (ProjectileType == null)
         {
             throw new UnassignedReferenceException("No projectile specified");
+        }
+    }
+
+    public void Shoot() {
+        var BasePosition = ProjectileSpawnLocation.transform.position;
+        var Offset = new Vector3(Random.Range(0, SpawnJitter) - SpawnJitter/2, Random.Range(0, SpawnJitter) - SpawnJitter/2, 0);
+
+        var BaseRotation = transform.rotation;
+        var RotationOffset = Quaternion.Euler(Random.Range(0, Inaccuracy) - Inaccuracy/2, Random.Range(0, Inaccuracy) - Inaccuracy/2, 0);
+        Instantiate(ProjectileType, BasePosition + Offset, BaseRotation * RotationOffset);
+        // Start the cooldown
+        CooldownIsActive = true;
+    }
+
+    void Update()
+    {
+        // Track time elapsed since cooldown started
+        if (CooldownIsActive) {
+            Cooldown += Time.deltaTime;
+        }
+
+        // Reset cooldown states
+        if (Cooldown > ShotDelay) {
+            Cooldown = 0.0f;
+            CooldownIsActive = false;
+        }
+
+        if (!CooldownIsActive && Input.GetButton("PrimaryFire")) {
+            // Allow for shooting
+            Shoot();
         }
     }
 }
