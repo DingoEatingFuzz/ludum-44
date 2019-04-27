29f9593a15f7db40cef966da71e2bc58331dfdea
Tilde <pxn.tilde@protonmail.com>
2019-04-26 23:44:25 -0700

    Added a static game state
    for transferring data across levels

---
+++ Assets/Scripts/GameManagement/GameStateComponent.cs
@@ -0,0 +1,55 @@
+﻿using Management.Persistent;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+
+
+
+public class GameStateComponent : MonoBehaviour
+{
+    public static GameStateComponent Instance;
+
+    protected PersistentData Data;
+
+    /// <summary>
+    /// Awake
+    /// </summary>
+    protected void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Capies 
+    /// </summary>
+    public void PrepareData(PersistentData Data, System.Action Callback)
+    {
+        this.Data = Data;
+    }
+}
+
+namespace Management.Persistent
+{
+    /// <summary>
+    /// Contains stuff that sticks around between levels
+    /// </summary>
+    [System.Serializable]
+    public struct PersistentData
+    {
+
+    }
+}
