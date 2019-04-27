c5dc6d5c628c52216ba44db38e510bfa9b2dcf88
ADKauth <adamkauth@gmail.com>
2019-04-27 16:23:26 -0700

    Basic camera following

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+//        transform.position = new Vector3(PlayerPosition.position.x, PlayerPosition.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, transform.position.z);
+    }
+}
