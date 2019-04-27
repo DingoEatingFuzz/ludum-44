d279a71bc73b61907e06540274acfd19d6fd3ee7
Michael Lange <dingoeatingfuzz@gmail.com>
2019-04-27 16:23:13 -0700

    Dialogue controls and triggers.

---
+++ Assets/Scripts/DialogueTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var DialogueManager = gameObject.GetComponent<DialogueManager>();
+        Coroutine dialogue;
+        if (!DialogueManager.IsOpen && Input.GetButtonDown("PayRespects")) {
+            dialogue = StartCoroutine(SpawnDialogue());
+        }
+    }
+
+    IEnumerator SpawnDialogue() {
+        var DialogueManager = gameObject.GetComponent<DialogueManager>();
+        yield return DialogueManager.Write("This is a test for");
+        yield return DialogueManager.Write("Speakonia");
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<string> Messages = new List<string>(){ "This is a test for Speakonia" };
+    public int MaxOccurrences = 1;
+    private int Occurrences = 0;
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// If the player collides with this trigger, start a dialogue sequence.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && Occurrences < MaxOccurrences) {
+            Occurrences++;
+            var dialogueManager = other.GetComponent<DialogueManager>();
+            StartCoroutine(RunThroughDialogue(dialogueManager));
+        }
+    }
+
+    IEnumerator RunThroughDialogue(DialogueManager dialogueManager) {
+        foreach (var message in Messages) {
+            yield return dialogueManager.Write(message);
+        }
+    }
+}
