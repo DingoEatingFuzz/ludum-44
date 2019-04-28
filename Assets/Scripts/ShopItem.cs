7c9f87f29cae5b8309da1a18cca12e0e45d8eebd
ADKauth <adamkauth@gmail.com>
2019-04-28 13:26:17 -0700

    Can buy items if enough health available

--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class ShopItem : MonoBehaviour
 {
+    public int ItemPrice;
+
     // Start is called before the first frame update
     List<SpriteRenderer> delays = new List<SpriteRenderer>();
     IEnumerator purchaseDelay;
-    Light light;
+    Light indicatorLight;
     float baseIntensity;
+    TextMesh priceText;
+    SpriteRenderer careCoinSymbol;
+    SpriteRenderer goneIcon;
+    bool available;
 
     void Start()
     {
@@ -16,8 +22,16 @@ public class ShopItem : MonoBehaviour
         delays.Add(transform.Find("delay2").GetComponent<SpriteRenderer>());
         delays.Add(transform.Find("delay3").GetComponent<SpriteRenderer>());
         ResetDelays();
-        light = transform.Find("light").GetComponent<Light>();
-        baseIntensity = light.intensity;
+        indicatorLight = transform.Find("light").GetComponent<Light>();
+        baseIntensity = indicatorLight.intensity;
+
+        priceText = transform.Find("price").GetComponent<TextMesh>();
+        priceText.text = ItemPrice + "cc";
+
+        careCoinSymbol = transform.Find("CareCoinSymbol").GetComponent<SpriteRenderer>();
+        goneIcon = transform.Find("goneIcon").GetComponent<SpriteRenderer>();
+        goneIcon.gameObject.SetActive(false);
+        available = true;
     }
 
     void ResetDelays() {
@@ -25,36 +39,52 @@ public class ShopItem : MonoBehaviour
     }
 
     void LightOn() {
-        light.intensity = baseIntensity * 2;
+        indicatorLight.intensity = baseIntensity * 2;
     }
 
     void LightOff() {
-        light.intensity = baseIntensity;
+        indicatorLight.intensity = baseIntensity;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player") {
-            purchaseDelay = BuyButSlowlyJustInCaseThePlayerChangesTheirMindOrDoesNotRealizeWhatIsHappening();
-            StartCoroutine(purchaseDelay);
-            LightOn();
+        if (available && other.tag == "Player")
+        {
+            if (other.gameObject.GetComponent<HealthComponent>().Current >= ItemPrice)
+            {
+                purchaseDelay = BuyButSlowlyJustInCaseThePlayerChangesTheirMindOrDoesNotRealizeWhatIsHappening(other.gameObject);
+                StartCoroutine(purchaseDelay);
+                LightOn();
+            } else
+            {
+                Debug.Log("Not enough moneys");
+            }
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player") {
+        if (other.tag == "Player" && purchaseDelay != null) {
             StopCoroutine(purchaseDelay);
             ResetDelays();
             LightOff();
         }
     }
 
-    IEnumerator BuyButSlowlyJustInCaseThePlayerChangesTheirMindOrDoesNotRealizeWhatIsHappening() {
-        foreach (var delay in delays) {
+    IEnumerator BuyButSlowlyJustInCaseThePlayerChangesTheirMindOrDoesNotRealizeWhatIsHappening(GameObject player) {
+        foreach (var delay in delays)
+        {
             yield return new WaitForSeconds(0.5f);
             delay.enabled = true;
         }
         Debug.Log("Buy the thing!");
+        player.GetComponent<HealthComponent>().Remove(ItemPrice);
+        player.GetComponent<PlayerController>().AddUpgrade("placeholder");
+        available = false;
+        goneIcon.gameObject.SetActive(true);
+        priceText.text = "";
+        var ccsColor = careCoinSymbol.color;
+        ccsColor.a = 0.0f;
+        careCoinSymbol.color = ccsColor;
     }
 }
