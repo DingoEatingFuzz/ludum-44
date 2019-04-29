e03a32f19904ce281802057ee1d67cfd7d91afbe
ADKauth <adamkauth@gmail.com>
2019-04-29 11:47:02 -0700

    Exit Portal changes scenes and stuff

---
+++ Assets/Scripts/ExitPortaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitPortaler : MonoBehaviour
+{
+    public string NextSceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameStateComponent>().StoreData();
+            SceneManager.LoadScene(NextSceneName);
+        }
+    }
+}
