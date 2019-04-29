74725378e9329f42a6c206a782ce16d891a4c6a3
vertigo <victor@nebulacore.net>
2019-04-29 03:30:21 -0700

    Button Works!

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Adam");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
