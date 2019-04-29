124f54b7a12ea19370ec2dfa462595f358900308
Tilde <pxn.tilde@protonmail.com>
2019-04-29 15:13:27 -0700

    Added enemy movement component

---
+++ Assets/Scripts/EnemyScripts/EnemyMovementComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementComponent : MonoBehaviour
+{
+    public float Movespeed = 8f;
+
+    [HideInInspector]
+    public Vector3 MoveTarget;
+
+    [HideInInspector]
+    public GameObject LookAt;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var Position = transform.position;
+        var Difference = Position - MoveTarget;
+        var MoveDirection = Difference.normalized;
+        
+        if (Difference.magnitude < Movespeed)
+        {
+            transform.position += MoveDirection * Movespeed;
+        }
+
+        if (LookAt != null)
+        {
+            var LookDirection = Position - LookAt.transform.position;
+            LookDirection.Normalize();
+            var Rotation = Quaternion.LookRotation(LookDirection, transform.up);
+        }
+    }
+}
