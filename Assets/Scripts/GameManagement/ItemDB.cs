b9776ba3f5ee2052410cc7a70463398371b02495
vertigo <victor@nebulacore.net>
2019-04-30 22:44:28 -0700

    Price Changes

--- Assets/Scripts/GameManagement/ItemDB.cs
+++ Assets/Scripts/GameManagement/ItemDB.cs
@@ -10,15 +10,15 @@ public class Item {
 public static class ItemDB
 {
   public static Dictionary<string, Item> Weapons = new Dictionary<string, Item>() {
-    { "laser", new Item { name = "Laser", price = 200 } },
-    { "gatling", new Item { name = "Super Gatling", price = 300 } },
-    { "plasma", new Item { name = "Plasma Cannon", price = 500 } },
+    { "laser", new Item { name = "Laser", price = 100 } },
+    { "gatling", new Item { name = "Super Gatling", price = 150 } },
+    { "plasma", new Item { name = "Plasma Cannon", price = 200 } },
   };
 
   public static Dictionary<string, Item> Upgrades = new Dictionary<string, Item>() {
-    { "healthUp", new Item { name = "+ Max Health", price = 200 } },
-    { "fireRateUp", new Item { name = "+ Fire Rate", price = 300 } },
-    { "damageUp", new Item { name = "+ Damage", price = 400 } },
+    { "healthUp", new Item { name = "+ Max Health", price = 100 } },
+    { "fireRateUp", new Item { name = "+ Fire Rate", price = 150 } },
+    { "damageUp", new Item { name = "+ Damage", price = 175 } },
   };
 
   public static Dictionary<string, Item> Charity = new Dictionary<string, Item>() {
