2b56da543c85b7d32553d422bdbd13824aae3c36
Tilde <pxn.tilde@protonmail.com>
2019-05-06 01:46:45 -0700

    Created a dialogue scriptable object and some helper classes

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    [System.Serializable]
+    //[CreateAssetMenu(fileName = "MovementData", menuName = "Scriptables/Movement/MovementData")]
+    [CreateAssetMenu(fileName = "Dialogue", menuName = "Scriptables/Dialogue/Dialogue")]
+    public class Dialogue : ScriptableObject
+    {
+        [Tooltip("Messages for this dialogue")]
+        public List<MessagePair> Messages;
+
+        [Tooltip("Priority to play")]
+        public Priority Priority = Priority.Normal;
+
+        [Tooltip("Can other dialogues interrupt this one?")]
+        public bool Interruptible = false;
+
+        [Tooltip("Should this dialogue continue after being interrupted?")]
+        public bool ResumeAfterInterrupt = false;
+
+        public Dialogue Clone()
+        {
+            Dialogue Clone = CreateInstance<Dialogue>();
+
+            Clone.Messages = Messages.Select(o => o).ToList();
+            Clone.Priority = Priority;
+            Clone.Interruptible = Interruptible;
+            Clone.ResumeAfterInterrupt = ResumeAfterInterrupt;
+
+            return Clone;
+        }
+    } 
+}
---
+++ Assets/Scripts/Dialogue/DialogueHelpers.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    /// <summary>
+    /// List of all the portraits
+    /// </summary>
+    public enum Character
+    {
+        None,
+        General,
+        MaxHart,
+        OlympusTower,
+        Shopkeep,
+        Unknown
+    }
+
+    public enum Priority
+    {
+        None,
+        Normal,
+        High
+    }
+
+    /// <summary>
+    /// Get the portrait from the character name
+    /// </summary>
+    public static class CharacterLookup
+    {
+        public static Dictionary<Character, Sprite> Characters =
+            new Dictionary<Character, Sprite>()
+            {
+                {Character.None, null },
+                {Character.General, Resources.Load<Sprite>("Portraits/MauraderGeneral") },
+                {Character.MaxHart, Resources.Load<Sprite>("Portraits/MaxHart") },
+                {Character.OlympusTower, Resources.Load<Sprite>("Portraits/OlympusTower") },
+                {Character.Shopkeep, Resources.Load<Sprite>("Portraits/ShopKeep") },
+                {Character.Unknown, Resources.Load<Sprite>("Portraits/Unknown") }
+            };
+    }
+
+    [System.Serializable]
+    public class MessagePair
+    {
+        public Character Character;
+        [TextArea]
+        public string Message;
+        
+        public Sprite CharacterSprite
+        {
+            get => CharacterLookup.Characters[Character];
+        }
+    }
+}
