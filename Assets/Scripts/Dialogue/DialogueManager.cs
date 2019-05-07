de17d5f08b2ad459af0aee428353ad21ef5a46d8
vertigo <victor@nebulacore.net>
2019-05-07 15:58:16 -0700

    Added Message Sound

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -6,10 +6,12 @@ using System.Linq;
 
 namespace DialogueSystem
 {
+    [RequireComponent(typeof(AudioSource))]
     [System.Serializable]
     public class DialogueManager : MonoBehaviour
     {
         // Start is called before the first frame update
+        public AudioClip NewMessage;
         public GameObject DialogueIndicatorCanvas;
         public GameObject DialogueCanvas;
         protected List<Dialogue> DialogueQueue = new List<Dialogue>();
@@ -17,6 +19,7 @@ namespace DialogueSystem
         public float WaitForInputTime = 7f;
         protected int QueueCount => DialogueQueue.Count;
         protected GameObject DialogueIndicator = null;
+        private AudioSource AudioSource;
 
         private bool _AdvanceScript;
         protected bool AdvanceScript
@@ -36,7 +39,6 @@ namespace DialogueSystem
         void Update()
         {
             AdvanceScript = Input.GetButton("PayRespects");
-            //Debug.Assert((DialogueQueue.Count == 0) == (DialogueCoroutine is null), "Something has gone wrong with the dialogue manager");
 
             #region Display the dialogue indicator if there's messages waiting and the dialogue coroutine isn't running
             if (!DialogueIndicator && QueueCount > 0 && DialogueCoroutine is null)
@@ -50,7 +52,6 @@ namespace DialogueSystem
                 {
                     MessagesWaiting.text = "Message Waiting [Space]";
                 }
-                Debug.Log($"Indicator created, should show {NumMessages.text}");
                 if (!DialogueIndicator.activeInHierarchy)
                 {
                     DialogueIndicator.SetActive(true);
@@ -79,13 +80,14 @@ namespace DialogueSystem
         /// <returns>True if dialogue was added</returns>
         public bool AddToQueue(Dialogue Dialogue, bool ResetDialogue = true)
         {
+            AudioSource = GetComponent<AudioSource>();
             var Added = false;
             if (DialogueQueue.Contains(Dialogue) == false)
             {
+                AudioSource.PlayOneShot(NewMessage);
                 Dialogue.ResetEnumerator();                
                 DialogueQueue.Add(Dialogue);
                 DialogueQueue = DialogueQueue.OrderByDescending(d => d.Priority).ToList();
-                //Debug.Log($"Added {Dialogue} to the queue, total dialogues is {DialogueQueue.Count}");
                 Added = true;
 
                 #region Update dialogue messages waiting indicator when new messages are received
@@ -100,14 +102,10 @@ namespace DialogueSystem
                             MessagesWaiting.text = "Messages Waiting [Space]";
                         }
                         NumMessages.text = QueueCount.ToString();
-                        Debug.Log($"Updated, should show {NumMessages.text}");
                         DialogueIndicator.SetActive(true);
                     }
                 }
                 #endregion
-
-                // Since we don't want it to run through the dialogue when stuff is added
-                // this should move somewhere else, replaced by the stuff you have below
             }
 
             return Added;
@@ -175,17 +173,7 @@ namespace DialogueSystem
 
                     yield return new WaitForSeconds(.5f);
 
-                    // Another condition can be added to the WaitUntil like a CloseDialogue bool 
                     yield return new WaitUntil(() => Time.time >= EndTime || AdvanceScript);
-
-                    // Then something here to determine if CloseDialogue was the reason, and if so
-                    // break the loop (which will not advance the dialogue and then close the canvas per
-                    // the Destroy below the while
-                    // E.g., if (CloseDialogue == true) { break; } (`break` tells the loop to abort)
-                    // If you WANT the message that is open to be discarded then you would remove it from the
-                    // CurrentMessage from CurrentDialogue (like in the "get the current message" region above)
-                    // If you want to abort the whole dialogue and not just the current message then you jsut
-                    // remove CurrentDialogue from the dialogue queue (like in the "Get the current dialogue" region above)
 
                 } while (DialogueQueue.Count > 0);
 
@@ -196,9 +184,6 @@ namespace DialogueSystem
 
         protected void PostDialogueCoroutine()
         {
-            // This is called when the dialogue is finished running, which normally would be when 
-            // the queue is empty, but will also now be when the dialogue is closed, so if there is still dialogue
-            // then you'd want to remake the indicator here (and update the message count text
             DialogueCoroutine = null;
         }
     }
